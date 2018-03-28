7c911f7bcb3b0846101350520a65d51b5f826b89
Colette Gallagher <10009693@gmit.ie>
2018-03-28 13:56:24 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,30 @@
-# Colette Gallagher, 2018-03-28
-# Excercise 6 Factorial
+# Colette Gallagher, 2018-03-25
+# Exercise 6 Factorial
+#https://www.youtube.com/watch?v=ZyYp1V84Xqc
 
-def Factorial(upto):
-    factorialupto=0
-    for i in range(1, upto+1):
-        factorialupto = factorialupto+1
-    return factorialupto
 
-print("The factorial of" ,factorial(5) "is" )    
+def factorial(number):
+    product = 1
+    for i in range(number):
+        product = product*(i+1)
+    return product
+
+number = 5
+
+int = number
+factorial_of_int = factorial(int)
+print("The factorial of", (int), "is ", (factorial_of_int))
+
+number = 7
+
+int = number
+factorial_of_int = factorial(int)
+print("The factorial of", (int), "is ", (factorial_of_int))
+
+number = 10
+
+int = number
+factorial_of_int = factorial(int)
+print("The factorial of", (int), "is ", (factorial_of_int))
+
 
