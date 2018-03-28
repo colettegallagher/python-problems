f118994372aaca355eb9d855d8843c612ba5b451
colettegallagher <36208701+colettegallagher@users.noreply.github.com>
2018-03-28 09:46:45 +0100

    Update collatz.py
    *The Collatz Conjecture
    The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half the previous term. Otherwise, the next term is 3 times the previous term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1. (https://en.wikipedia.org/wiki/Collatz_conjecture)
    
    **Exercise 3
    This exercise contains a Python script which starts with an integer.  The Collatz Function is then repeatedly applied (divide by 2 if even, multiply by three and add one if odd)
    
    The starting value has been given as 17.  The current value of the integer is printed to the screen at each iteration.

--- collatz.py
+++ collatz.py
@@ -2,9 +2,16 @@
 # URL: https://en.wikipedia.org/wiki/Collatz_conjecture
 
 n = 17
+# Starting integer is 17
+
 while n > 1:
   if n % 2==0:
     n = n / 2
+# Divide by 2 if the integer is Even
+
   else: 
     n = 3 * n + 1
+# Multiple by 3 and add 1 if the integer is Odd
+
   print (n)
+# Print at each iteration
