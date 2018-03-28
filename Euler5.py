09f3e84f2dafc143d6459242fad8e178155066c5
colettegallagher <36208701+colettegallagher@users.noreply.github.com>
2018-03-28 10:34:51 +0100

    Update Euler5.py

--- Euler5.py
+++ Euler5.py
@@ -1,8 +1,11 @@
 # Colette Gallagher, 2018-02-28
 # https://projecteuler.net/problem=5
+# https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 
 def check(n):
     for i in range(11, 21):
+# use only numbers 11 to 20 as we know 2520 is the smallest number divided by 1 to 10  
+
         if n % i == 0:
             continue
         else:
@@ -10,8 +13,9 @@ def check(n):
     return True
     
 x = 2520
-
+ # we know 2520 is the smallest number that can be divided by 1 to 10 so we can start from 2520 to shorten the calculation time
+    
 while not check(x):
     x += 2520
-    
+   
 print(x)
