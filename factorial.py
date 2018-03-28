f0978fa439d5d7b61d0930ec8685c9f6676708c6
Colette Gallagher <10009693@gmit.ie>
2018-03-28 14:41:10 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,37 +2,27 @@
 # Exercise 6 Factorial
 # https://en.wikipedia.org/wiki/Factorial
 # https://www.youtube.com/watch?v=ZyYp1V84Xqc
+# https://nbviewer.jupyter.org/github/ianmcloughlin/python-fundamentals-notes/blob/master/functions-modules.ipynb#
 
-# The factorial of a number is that number multiplied by all of the positive numbers less than it
+# The factorial of a number is that number multiplied by all of the positive numbers less than that number
 
 
-def factorial(number):
-    product = 1
-    for i in range(number):
-        product = product*(i+1)
-    return product
+def factorial(n):
+# Returns the factorial of n
+    ans = 1
+    for i in range(2, n + 1):
+        ans = ans * i
+    return ans
 # defines the function of the factorial
 
+x=5
+y=7
+z=10
+# Gives values to x,y,z
 
-number = 5
-
-int = number
-factorial_of_int = factorial(int)
-print("The factorial of", (int), "is ", (factorial_of_int))
-# prints the factorial of the number 5 which equals 120
-
-number = 7
-
-int = number
-factorial_of_int = factorial(int)
-print("The factorial of", (int), "is ", (factorial_of_int))
-# prints the factorial of the number 7 which equals 5040
-
-number = 10
-
-int = number
-factorial_of_int = factorial(int)
-print("The factorial of", (int), "is ", (factorial_of_int))
-# prints the factorial of the number 10 which equals 3628800
+print("The Factorial of", (x), "is ", factorial(x))
+print("The Factorial of", (y), "is ", factorial(y))
+print("The Factorial of", (z), "is ", factorial(z))  
+# Prints the Factorials of the numbers 5, 7, 10 to give the factorials 120, 5040, 3628800 respectively
 
 
