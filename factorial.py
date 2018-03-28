160c4e2279f8729356188a8794a325f9ea2691ac
Colette Gallagher <10009693@gmit.ie>
2018-03-28 14:14:42 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,9 @@
 # Colette Gallagher, 2018-03-25
 # Exercise 6 Factorial
-#https://www.youtube.com/watch?v=ZyYp1V84Xqc
+# https://en.wikipedia.org/wiki/Factorial
+# https://www.youtube.com/watch?v=ZyYp1V84Xqc
+
+# The factorial of a number is that number multiplied by all of the positive numbers less than it
 
 
 def factorial(number):
@@ -8,23 +11,27 @@ def factorial(number):
     for i in range(number):
         product = product*(i+1)
     return product
+# defines the function of the factorial
+
 
 number = 5
 
 int = number
 factorial_of_int = factorial(int)
 print("The factorial of", (int), "is ", (factorial_of_int))
+# prints the factorial of the number 5 which equals 120
 
 number = 7
 
 int = number
 factorial_of_int = factorial(int)
 print("The factorial of", (int), "is ", (factorial_of_int))
+# prints the factorial of the number 7 which equals 5040
 
 number = 10
 
 int = number
 factorial_of_int = factorial(int)
 print("The factorial of", (int), "is ", (factorial_of_int))
-
+# prints the factorial of the number 10 which equals 3628800
 
