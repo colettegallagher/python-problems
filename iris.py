91c3b4002f0c40ae51b09e648651ca0b77b86cda
colettegallagher <36208701+colettegallagher@users.noreply.github.com>
2018-03-28 11:33:43 +0100

    Update iris.py
    Exercise 5 required the conversion of the Iris Flower Data set to a tabular format with headings.
    
    **References:**
    1. Explanation of [Iris Flower Data Set](https://en.wikipedia.org/wiki/Iris_flower_data_set) and its creator [Ronald Fisher](https://en.wikipedia.org/wiki/Ronald_Fisher)
    2. Link to [Iris Data Set](http://archive.ics.uci.edu/ml/datasets/Iris) for download as a .csv file to Visual Studio Code.

--- iris.py
+++ iris.py
@@ -1,8 +1,13 @@
-# Colette Gallagher, 19/03/18
-# Excercise 5 Iris Data Set
+# Colette Gallagher, 2018-03-19
+# Exercise 5 Iris Data Set
+# https://docs.python.org/2/library/string.html
 
 print ("petal length ", "petal width ", "sepal length ", "sepal width")
+# Prints the Iris Dataset header names 
 
 with open ("data/iris.csv") as f:
+# Opens the iris data set which must be saved as a .csv file on visual studio code, link in comment box
+
   for line in f:
     print ("{:>10} {:>10} {:>10} {:>10}" .format(line.split(',')[0],line.split(',')[1],line.split(',')[2],line.split(',')[3]))
+# Splits the string and formats each entry into tabular format
